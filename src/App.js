112d6c84ff53a91e4d377bf774ce21ae3b252e9f
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-09-20 19:03:21 -0600

    Add handler function and reset button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+
+  const handler = (amount, teamName) => {
+    return teamName === 'Lions' ? setHomeScore(homeScore + amount) : setAwayScore(awayScore + amount);
+  }
+
+  const reset = () => {
+    setHomeScore(0);
+    setAwayScore(0);
+  }
+  
   return (
     <div className="container">
       <section className="scoreboard">
@@ -29,12 +39,15 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => {setHomeScore(homeScore + 7)}}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => {setHomeScore(homeScore + 3)}}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => {handler(7, 'Lions')}}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => {handler(3, 'Lions')}}>Home Field Goal</button>
+        </div>
+        <div>
+          <button className='reset' onClick={() => {reset()}}>Reset</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => {setAwayScore(awayScore + 7)}}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => {setAwayScore(awayScore + 3)}}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => {handler(7, 'Tigers')}}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => {handler(3, 'Tigers')}}>Away Field Goal</button>
         </div>
       </section>
     </div>
